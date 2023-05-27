99f39d901e7063a9ace7300e012f5f853b6acfa6
juber <juberk1010@gmail.com>
2023-05-27 14:44:12 +0530

    make filter method function for filter out the user by the name and age

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import '../login.css'
+const Login = () => {
+ const navigate = useNavigate();
+ const [userEmail , setUserEmail] = useState('');
+ const [userPassword , setUserPassword] = useState('');
+ const [errorStatus , setErrorStatus] = useState(false);
+//  demo@coralmango.com
+// demo123
+
+const userCredential = {
+  email :  "demo@coralmango",
+  password : "demo123" 
+}
+
+const handleLoginForm = (e) => {
+   e.preventDefault();
+   if(userEmail === userCredential.email && userPassword === userCredential.password){
+    setErrorStatus(false);
+    // alert("success");
+    navigate('/users');
+   }else{
+    setErrorStatus(true);
+   }
+ }
+  return (
+    <div className="form-container">
+      <div className='login-form'>
+        <h3>Login</h3>
+        <form autoComplete='off' onSubmit={(e) => handleLoginForm(e)}>
+          <input type="text" placeholder='Email' onChange={(e) => setUserEmail(e.target.value)}/>
+          <input type="password" placeholder='Password' onChange={(e) => setUserPassword(e.target.value)}/>
+          <button type='submit'>Submit</button>
+        </form>
+          {errorStatus ? <small className='error'>Invalid Credentials!</small> : ""}
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Users.js
@@ -0,0 +1,122 @@
+import React, { useEffect, useState } from "react";
+
+const Nav = ({ toggle, setToggle }) => {
+  return (
+    <header className="header">
+      <button className="toggle-btn" onClick={() => setToggle(!toggle)}>
+        {toggle  ? "Show in Table" : "Show in Card"}
+      </button>
+    </header>
+  );
+};
+
+const Card = ({ user }) => {
+  return (
+    <div className="user-card">
+      <div className="user-info">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtF3o2PvqxOMHgdrpj_YRItsLBjxyTeNZu_Q&usqp=CAU"
+          alt="user-avtar"
+        />
+        <span className="user-name">{user.name}</span>
+        <span className="user-occupation">{user.occupation}</span>
+        <span className="user-age">{user.age}</span>
+      </div>
+    </div>
+  );
+};
+
+const Table = ({data}) => {
+  return (
+    <div className="table-container">
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+          <th>Occupation</th>
+        </tr>
+        {data.map((user) => (
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.age}</td>
+            <td>{user.occupation}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
+};
+
+const Users = () => {
+  const [userData, setUserData] = useState([]);
+  const [loding, setLoding] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [filterValue, setfilterValue] = useState("");
+
+  const getUser = async () => {
+    try {
+      setLoding(true);
+      const res = await fetch("https://coralmango.com/api/react-test");
+      const data = await res.json();
+      setUserData(data);
+      setLoding(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const filterUser = () => {
+    return userData.filter((user) => user.name.toLowerCase().includes(inputValue) || user.age === inputValue);
+  }
+
+  const filterTable = (e) => {
+    setfilterValue(e.target.value)
+    if(filterValue === "age"){
+      const numAscending = [...userData].sort((a, b) => a.age - b.age);
+      setUserData(numAscending);    
+    }
+    if(filterValue === "name") {
+      const strAscending = [...userData].sort((a, b) => a.name > b.name ? 1 : -1);
+      setUserData(strAscending);
+    }
+  }
+
+
+  return (
+    <>
+      <Nav toggle={toggle} setToggle={setToggle} />
+      <div className="container">
+        <div className="filter-container">
+        <div className="radio-btns">
+          <small style={{marginRight: "1rem"}}>Filter</small>
+           <select className="filter-option" onChange={(e) => filterTable(e)}>
+            <option value="" selected>--select--</option>
+            <option value="name">Filter By Age</option>
+            <option value="age">Filter By Name</option>
+           </select>
+        </div>
+          {
+            inputValue.length > 0 ? <div className="filter-view-message">You Are viewing filtered results!</div> : ""
+
+          }
+
+        </div>
+        <div className="search_container">
+          <input type="search" value={inputValue} placeholder="Enter name or age" onChange={(e) => setInputValue(e.target.value)}/>
+        </div>
+        {loding ? <p>Loding....</p> : 
+          <div className="user-container">
+            {!toggle ? <Table data={filterUser(userData)}/> : filterUser(userData).map((user , index) => <Card user={user} key={index}/>)}
+          </div>
+        }
+      </div>
+    </>
+  );
+};
+
+export default Users;
